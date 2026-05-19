c28d39fe9d78d96fdb3ddbd01b1cc1824d4e9d5f
delbraeth <patrick.cassidy@mac.com>
2026-05-19 09:06:45 -0400

    Remove stale deploy script fix-and-push.sh (replaced by _deploy.py).

--- fix-and-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Remove git lock file and push to GitHub
-cd "$(dirname "$0")"
-rm -f .git/index.lock
-echo "Lock removed."
-git push origin main
-echo "Done! Check https://delbraeth.github.io/swim-workout-generator"
-read -p "Press Enter to close..."
